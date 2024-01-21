b5a4f83d24f88fdda5db92df784028702d70606e
unknown <souhaielhamdi38@gmail.com>
2024-01-21 21:08:19 +0100

    Scroll Project

---
+++ main.js
@@ -0,0 +1,48 @@
+// get the elements by Id
+
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains3 = document.getElementById('mountains3');
+let mountains4 = document.getElementById('mountains4');
+let river = document.getElementById('river');
+let boat = document.getElementById('boat');
+let nouvil = document.querySelector('.nouvil');
+
+
+// get translate the stars with scroll method
+
+window.onscroll=function()
+{
+    let value = scrollY;
+    stars.style.left= value + 'px';
+    moon.style.top = value *4 + 'px';
+    mountains3.style.top = value * 2 + 'px';
+    mountains4.style.top = value * 1.5 + 'px';
+    river.style.top = value + 'px';
+    boat.style.top= value  + 'px';
+    boat.style.left = value * 3 + 'px';
+    nouvil.style.fontSize = value + 'px';
+    if(scrollY >= 68)
+    {
+        nouvil.style.fontSize = 68 + 'px';
+        nouvil.style.position = 'fixed';
+
+        if(scrollY >= 449)
+        {
+            nouvil.style.display = 'none'
+        }
+        else{
+            nouvil.style.display = 'block'
+        }
+
+        if(scrollY >= 102)
+        {
+            nouvil.innerHTML='good morning';
+            document.querySelector('.main').style.background = 'linear-gradient(#376281, #10001f)'
+
+        }
+        else{
+            document.querySelector('.main').style.background = 'linear-gradient(#200016,#10001f)'
+        }
+    }
+}
